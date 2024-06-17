2ba6393cf6f2c584927f837cc5d907f3a2db286f
oleksandr_zamuruiev <zamuruev@i.ua>
2024-06-17 16:04:49 +0300

    update json

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,29 +10,6 @@ export function renderGallery(images) {
       <img src="${image.webformatURL}" alt="${image.tags}" class="gallery__image"/>
       <div class="description">${image.tags}</div>
       <div class="info">
-        <p class="info-item"><b>Likes</b>${image.likes}</p>
-        <p class="info-item"><b>Views</b>${image.views}</p>
-        <p class="info-item"><b>Comments</b>${image.comments}</p>
-        <p class="info-item"><b>Downloads</b>${image.downloads}</p>
-      </div>
-    </a>`
-    )
-    .join('');
-  const lightbox = new SimpleLightbox('.gallery a');
-  lightbox.refresh();
-}
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-export function renderGallery(images) {
-  const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = images
-    .map(
-      image => `
-    <a href="${image.largeImageURL}" class="gallery__link">
-      <img src="${image.webformatURL}" alt="${image.tags}" class="gallery__image"/>
-      <div class="description">${image.tags}</div>
-      <div class="info">
         <div class="info-item">
           <b>Likes</b>
           <span>${image.likes}</span>
